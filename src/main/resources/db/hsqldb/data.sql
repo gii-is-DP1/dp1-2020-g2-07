a5f36f67983f919cf65720bf2deca76a6a9d7f45
djd16 <Diego@DESKTOP-8B6TDJI>
2020-11-15 20:11:38 +0100

    Falla el edit y botón de añadir

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -68,5 +68,5 @@ INSERT INTO visits(id,pet_id,visit_date,description) VALUES (2, 8, '2013-01-02',
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (3, 8, '2013-01-03', 'neutered');
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (4, 7, '2013-01-04', 'spayed');
 
-INSERT INTO bono(id, precio, duracion) VALUES (1,10,3);
-INSERT INTO bono(id, precio, duracion) VALUES (2,3,1);
+INSERT INTO bonos(id, precio, duracion) VALUES (1,10,3);
+INSERT INTO bonos(id, precio, duracion) VALUES (2,3,1);
