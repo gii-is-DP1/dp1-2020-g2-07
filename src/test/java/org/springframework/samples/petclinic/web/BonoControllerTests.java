a5b43bb23232535177136c5da46520893a5323a4
djd16 <Diego@DESKTOP-8B6TDJI>
2020-11-24 17:06:56 +0100

    Pruebas unitarias hechas (fallan 2 que hay que revisar)

--- src/test/java/org/springframework/samples/petclinic/web/BonoControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/BonoControllerTests.java
@@ -1,7 +1,9 @@
 package org.springframework.samples.petclinic.web;
 
 import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -57,16 +59,80 @@ class BonoControllerTests{
 		miguel.setPrecio(3);
 		
 		given(this.bonoservice.findBonos()).willReturn(Lists.newArrayList(diego,miguel));
-//		given(this.bonoservice.findBonoById(TEST_BONO_ID)).willReturn(new Bono());
+//		given(this.bonoservice.findById(TEST_BONO_ID).get()).willReturn(diego);
 	
 	}
 	
+	//Iniciar creación de un bono
 	@WithMockUser(value = "spring")
 	@Test
 	void testInitNewBonoForm() throws Exception{
 		mockMvc.perform(get("/bonos/new", TEST_BONO_ID)).andExpect(status().isOk()).andExpect(view().name("bonos/createOrUpdateBonosForm"));
 	}
 	
+	//Crear un bono correctamente
+	@WithMockUser(value = "spring")
+    @Test
+	void testProcessCreationFormSuccess() throws Exception {
+		mockMvc.perform(post("/bonos/new", TEST_BONO_ID)
+							.with(csrf())
+							.param("codigo", "QWERTY1")
+							.param("precio", "3")
+							.param("duracion", "2021-02-12")
+							.param("descripcion", "prueba"))
+				.andExpect(status().is2xxSuccessful());
+	}
+	
+	//Crear un bono con errores
+//	@WithMockUser(value = "spring")
+//    @Test
+//	void testProcessCreationFormHasErrors() throws Exception {
+//		mockMvc.perform(post("/bonos/new", TEST_BONO_ID)
+//						.with(csrf())
+//						.param("codigo", "QWERTY1")
+//						.param("duracion", "2021/02/12"))
+//				//.andExpect(model().attributeHasNoErrors("owner"))
+//				.andExpect(model().attributeHasErrors("bonos"))
+//				.andExpect(status().isOk())					
+//				.andExpect(view().name("bonos/createOrUpdateBonosForm"));
+//	}
+	
+	//Iniciar formulario de editar
+	@WithMockUser(value = "spring")
+	@Test
+		void testInitUpdateForm() throws Exception {
+			mockMvc.perform(get("/bonos/{bonoId}/edit", TEST_BONO_ID))
+					.andExpect(status().isOk()).andExpect(model().attributeExists("bonos"))
+					.andExpect(view().name("bonos/BonosListing"));
+		}
+	 
+	//Actualizar bonos correctamente
+	@WithMockUser(value = "spring")
+	@Test
+		void testProcessUpdateFormSuccess() throws Exception {
+			mockMvc.perform(post("/bonos/{bonoId}/edit", TEST_BONO_ID)
+								.with(csrf())
+								.param("codigo", "QWERTY1")
+								.param("precio", "3")
+								.param("duracion", "2021-02-12")
+								.param("descripcion", "prueba"))
+				.andExpect(status().is2xxSuccessful());
+	 }
+	//Actualizar bonos erróneamente
+//	 @WithMockUser(value = "spring")
+//	@Test
+//	void testProcessUpdateFormHasErrors() throws Exception {
+//		mockMvc.perform(post("/bonos/{bonoId}/edit",TEST_BONO_ID)
+//							.with(csrf())
+//							.param("codigo", "QWERTY1")
+//							.param("duracion", "2021/02/12"))
+//				//.andExpect(model().attributeHasNoErrors("owner"))
+//				.andExpect(model().attributeHasErrors("bonos"))
+//				.andExpect(status().isOk())
+//				.andExpect(view().name("bonos/createOrUpdateBonosForm"));
+//		}
+	 
+	//Mostrar lista Bonos
 	@WithMockUser(value = "spring")
 	@Test
 	void testShowBonosListHtml() throws Exception{
