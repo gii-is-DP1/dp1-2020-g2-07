cd9beb743a6cfa6aa9fdbfb45936efe320c0b6d5
djd16 <diego_16_rey@hotmail.com>
2020-11-28 19:41:41 +0100

    Fixed mostrar codigo en editar

--- src/main/java/org/springframework/samples/petclinic/web/BonoController.java
+++ src/main/java/org/springframework/samples/petclinic/web/BonoController.java
@@ -50,6 +50,7 @@ public class BonoController {
 		if(binding.hasErrors()) {
 			return BONOS_FORM;
 		}else {
+			modifiedBono.setCodigo();
 			BeanUtils.copyProperties(modifiedBono, bono.get(),"id");
 			bonoservice.save(bono.get());
 			model.addAttribute("message", "Bono ha sido actualizado");
