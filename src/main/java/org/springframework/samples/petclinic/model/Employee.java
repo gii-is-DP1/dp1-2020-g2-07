f041842578d13e9c8f6e14865d6ce350712dbe44
FranciscoJRE14 <frarodele@alum.us.es>
2021-01-24 19:32:08 +0100

    before logs

--- src/main/java/org/springframework/samples/petclinic/model/Employee.java
+++ src/main/java/org/springframework/samples/petclinic/model/Employee.java
@@ -25,10 +25,13 @@ public class Employee extends Individual {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "employee")
     private List<Horario> horarios;
+//    
+//    @ManyToMany(mappedBy="incomest", cascade = CascadeType.ALL)
+//	private List<Balance> incomest;
     
-    /*@ManyToMany(mappedBy="incomest", cascade = CascadeType.ALL)
-	private List<Balance> incomest;*/
-
+    @ManyToMany(cascade = CascadeType.ALL)
+	private List<Balance> incomest;
+   
     public List<Horario> getHorarios() {
 		return horarios;
 	}
