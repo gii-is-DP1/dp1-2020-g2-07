e1e08f661276929c3248a3ae338a2a514896e2cf
ZeldaXD <10564736+ZeldaXD@users.noreply.github.com>
2021-02-04 16:51:10 +0100

    Pruebas mutantes y assertj

---
+++ src/test/java/org/springframework/samples/petclinic/assertj-assertions/org/springframework/samples/petclinic/service/BddAssertions.java
@@ -0,0 +1,290 @@
+package org.springframework.samples.petclinic.service;
+
+/**
+ * Entry point for BDD assertions of different data types.
+ */
+@javax.annotation.Generated(value="assertj-assertions-generator")
+public class BddAssertions {
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.AdminMockTestAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.AdminMockTestAssert then(org.springframework.samples.petclinic.service.AdminMockTest actual) {
+    return new org.springframework.samples.petclinic.service.AdminMockTestAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.AdminServiceAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.AdminServiceAssert then(org.springframework.samples.petclinic.service.AdminService actual) {
+    return new org.springframework.samples.petclinic.service.AdminServiceAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.AuthoritiesServiceAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.AuthoritiesServiceAssert then(org.springframework.samples.petclinic.service.AuthoritiesService actual) {
+    return new org.springframework.samples.petclinic.service.AuthoritiesServiceAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.BalanceMockTestAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.BalanceMockTestAssert then(org.springframework.samples.petclinic.service.BalanceMockTest actual) {
+    return new org.springframework.samples.petclinic.service.BalanceMockTestAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.BalanceServiceAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.BalanceServiceAssert then(org.springframework.samples.petclinic.service.BalanceService actual) {
+    return new org.springframework.samples.petclinic.service.BalanceServiceAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.BonoServiceAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.BonoServiceAssert then(org.springframework.samples.petclinic.service.BonoService actual) {
+    return new org.springframework.samples.petclinic.service.BonoServiceAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.CircuitMockTestAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.CircuitMockTestAssert then(org.springframework.samples.petclinic.service.CircuitMockTest actual) {
+    return new org.springframework.samples.petclinic.service.CircuitMockTestAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.CircuitoServiceAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.CircuitoServiceAssert then(org.springframework.samples.petclinic.service.CircuitoService actual) {
+    return new org.springframework.samples.petclinic.service.CircuitoServiceAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.CitaMockedServiceTestsAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.CitaMockedServiceTestsAssert then(org.springframework.samples.petclinic.service.CitaMockedServiceTests actual) {
+    return new org.springframework.samples.petclinic.service.CitaMockedServiceTestsAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.CitaServiceAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.CitaServiceAssert then(org.springframework.samples.petclinic.service.CitaService actual) {
+    return new org.springframework.samples.petclinic.service.CitaServiceAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.ClientMockTestAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.ClientMockTestAssert then(org.springframework.samples.petclinic.service.ClientMockTest actual) {
+    return new org.springframework.samples.petclinic.service.ClientMockTestAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.ClienteServiceAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.ClienteServiceAssert then(org.springframework.samples.petclinic.service.ClienteService actual) {
+    return new org.springframework.samples.petclinic.service.ClienteServiceAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.EmailMockTestAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.EmailMockTestAssert then(org.springframework.samples.petclinic.service.EmailMockTest actual) {
+    return new org.springframework.samples.petclinic.service.EmailMockTestAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.EmailServiceAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.EmailServiceAssert then(org.springframework.samples.petclinic.service.EmailService actual) {
+    return new org.springframework.samples.petclinic.service.EmailServiceAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.EmployeeMockTestAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.EmployeeMockTestAssert then(org.springframework.samples.petclinic.service.EmployeeMockTest actual) {
+    return new org.springframework.samples.petclinic.service.EmployeeMockTestAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.EmployeeServiceAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.EmployeeServiceAssert then(org.springframework.samples.petclinic.service.EmployeeService actual) {
+    return new org.springframework.samples.petclinic.service.EmployeeServiceAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.HorarioMockedServiceTestsAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.HorarioMockedServiceTestsAssert then(org.springframework.samples.petclinic.service.HorarioMockedServiceTests actual) {
+    return new org.springframework.samples.petclinic.service.HorarioMockedServiceTestsAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.HorarioServiceAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.HorarioServiceAssert then(org.springframework.samples.petclinic.service.HorarioService actual) {
+    return new org.springframework.samples.petclinic.service.HorarioServiceAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.RoomMockTestAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.RoomMockTestAssert then(org.springframework.samples.petclinic.service.RoomMockTest actual) {
+    return new org.springframework.samples.petclinic.service.RoomMockTestAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.SalaServiceAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.SalaServiceAssert then(org.springframework.samples.petclinic.service.SalaService actual) {
+    return new org.springframework.samples.petclinic.service.SalaServiceAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.TokenMockTestAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.TokenMockTestAssert then(org.springframework.samples.petclinic.service.TokenMockTest actual) {
+    return new org.springframework.samples.petclinic.service.TokenMockTestAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.UserMockTestAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.UserMockTestAssert then(org.springframework.samples.petclinic.service.UserMockTest actual) {
+    return new org.springframework.samples.petclinic.service.UserMockTestAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.UserServiceAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.UserServiceAssert then(org.springframework.samples.petclinic.service.UserService actual) {
+    return new org.springframework.samples.petclinic.service.UserServiceAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.exceptions.DuplicatedCircuitoNameExceptionAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.exceptions.DuplicatedCircuitoNameExceptionAssert then(org.springframework.samples.petclinic.service.exceptions.DuplicatedCircuitoNameException actual) {
+    return new org.springframework.samples.petclinic.service.exceptions.DuplicatedCircuitoNameExceptionAssert(actual);
+  }
+
+  /**
+   * Creates a new instance of <code>{@link org.springframework.samples.petclinic.service.exceptions.DuplicatedSalaNameExceptionAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static org.springframework.samples.petclinic.service.exceptions.DuplicatedSalaNameExceptionAssert then(org.springframework.samples.petclinic.service.exceptions.DuplicatedSalaNameException actual) {
+    return new org.springframework.samples.petclinic.service.exceptions.DuplicatedSalaNameExceptionAssert(actual);
+  }
+
+  /**
+   * Creates a new <code>{@link BddAssertions}</code>.
+   */
+  protected BddAssertions() {
+    // empty
+  }
+}
